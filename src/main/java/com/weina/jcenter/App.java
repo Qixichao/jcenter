91e3b49785d5464cb832c4375f6b0c26919cbe64
qixichao <qixichao@163.com>
2015-06-26 23:56:31 +0800

    Initialize version

---
+++ src/main/java/com/weina/jcenter/App.java
@@ -0,0 +1,13 @@
+package com.weina.jcenter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
